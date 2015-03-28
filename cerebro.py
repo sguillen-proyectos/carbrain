0be50af2dff3ddea61b2bd23e49bb9d43109fc14
donkeysharp <serguimant@gmail.com>
2015-03-28 12:25:38 -0400

    commit 4

--- cerebro.py
+++ cerebro.py
@@ -1,20 +1,20 @@
 from carlog import logger
-# import RPi.GPIO as gpio
 import time
+import RPi.GPIO as gpio
+
 try:
-  pass
-  # gpio.setmode(gpio.BCM)
+  gpio.setmode(gpio.BCM)
 except:
   logger.info('error gpio setmode')
+
 class ManejaAuto():
   def __init__(self):
     try:
-      pass
-      # gpio.setmode(gpio.BCM)
-      # gpio.setup(17, gpio.OUT)
-      # gpio.setup(22, gpio.OUT)
-      # gpio.output(17, False)
-      # gpio.output(22, False)
+      gpio.setmode(gpio.BCM)
+      gpio.setup(17, gpio.OUT)
+      gpio.setup(22, gpio.OUT)
+      gpio.output(17, False)
+      gpio.output(22, False)
     except:
       logger.info('error gpio')
 
@@ -23,12 +23,12 @@ class ManejaAuto():
     try:
       if comando == '1':
         pass
-        # gpio.output(17, True)
-        # gpio.output(22, False)
+        gpio.output(17, True)
+        gpio.output(22, False)
       elif comando == '2':
         pass
-        # gpio.output(17, False)
-        # gpio.output(22, True)
+        gpio.output(17, False)
+        gpio.output(22, True)
     except:
       logger.info('error gpio output')
 
